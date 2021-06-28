e941898f93e04d40a0ed1f50278239967fd0ceed
shogo4131 <shougo80697118@gmail.com>
2021-06-29 00:30:57 +0900

    バリデーションチェック実装

--- React-CURD/Backend/src/app/routers/userListApi.js
+++ React-CURD/Backend/src/app/routers/userListApi.js
@@ -20,13 +20,19 @@ router.get('/api/v1/users', (req, res) => {
 
 /* 特定のユーザー情報を取得 */
 router.get('/api/v1/users/:id', (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return res.status(400).json({ errorMessage: '指定されたidがありません' });
+  }
+
   // DB接続
   const db = new sqlite3.Database(dbPath);
-  const id = req.params.id;
 
   try {
     db.get(
-      `select id, name, profile from users where id = ${id}`,
+      'select id, name, profile from users where id = ?',
+      [id],
       (err, row) => {
         row
           ? res.status(200).json(row)
@@ -42,64 +48,69 @@ router.get('/api/v1/users/:id', (req, res) => {
 
 /* ユーザー情報を登録 */
 router.post('/api/v1/users', async (req, res) => {
-  if (!req.body.name || req.body.name === '') {
-    res.status(400).json({ message: 'ユーザー名が指定されていません。' });
-  } else {
-    // Connect database
-    const db = new sqlite3.Database(dbPath);
+  const { name, profile } = req.body;
 
-    const name = req.body.name;
-    const profile = req.body.profile ? req.body.profile : '';
+  if (!id) {
+    return res.status(400).json({ errorMessage: '指定されたidがありません' });
+  }
 
-    try {
-      await run(
-        `INSERT INTO users (name, profile) VALUES ("${name}", "${profile}")`,
-        db
-      );
-      res.status(200).json({ message: '新規ユーザーを作成しました。' });
-    } catch (e) {
-      res.status(500).json({ error: 'ユーザーの登録に失敗しました' }, e);
-    } finally {
-      db.close();
-    }
+  if (!name || name.length > 10) {
+    return res.status(400).json({ errorMessage: '名前が入力されていません' });
+  }
+
+  if (!profile || profile.length > 30) {
+    return res
+      .status(400)
+      .json({ errorMessage: 'プロフィールが入力されていません' });
+  }
+
+  /* データベース接続 */
+  const db = new sqlite3.Database(dbPath);
+
+  try {
+    await run(
+      `INSERT INTO users (name, profile) VALUES ("${name}", "${profile}")`,
+      db
+    );
+    res.status(200).json({ message: '新規ユーザーを作成しました。' });
+  } catch (e) {
+    res.status(500).json({ error: 'ユーザーの登録に失敗しました' }, e);
+  } finally {
+    db.close();
   }
 });
 
 /* ユーザー情報を更新 */
 router.put('/api/v1/users/:id', async (req, res) => {
-  if (!req.body.name || req.body.name === '') {
-    res
-      .status(400)
-      .json({ message: 'ユーザー名またはプロフィールが入力されていません' });
-  } else {
-    const db = new sqlite3.Database(dbPath);
-    const id = req.params.id;
+  const { id } = req.params;
+  const { name, profile } = req.body;
 
-    db.get(`select * from users where id = ${id}`, async (err, row) => {
-      if (row) {
-        const name = req.body.name;
-        const profile = req.body.profile ? req.body.profile : '';
-
-        try {
-          await run(
-            `update users set name="${name}", profile="${profile}" where id = ${id}`,
-            db
-          );
-          res.status(200).json({ message: 'ユーザーの更新に成功しました' });
-        } catch (e) {
-          res
-            .status(500)
-            .json({ errorMessage: 'ユーザーの更新に失敗しました' });
-        } finally {
-          db.close();
-        }
-      } else {
-        res
-          .status(404)
-          .json({ message: '該当のユーザーが見つかりませんでした' });
-      }
-    });
+  if (!name || name.length > 10) {
+    return res.status(400).json({ errorMessage: '名前が入力されていません' });
   }
+
+  if (!profile || profile.length > 30) {
+    return res.status(400).json;
+  }
+  const db = new sqlite3.Database(dbPath);
+
+  db.get(`select * from users where id = ${id}`, async (err, row) => {
+    if (row) {
+      try {
+        await run(
+          `update users set name="${name}", profile="${profile}" where id = ${id}`,
+          db
+        );
+        res.status(200).json({ message: 'ユーザーの更新に成功しました' });
+      } catch (e) {
+        res.status(500).json({ errorMessage: 'ユーザーの更新に失敗しました' });
+      } finally {
+        db.close();
+      }
+    } else {
+      res.status(404).json({ message: '該当のユーザーが見つかりませんでした' });
+    }
+  });
 });
 
 /* ユーザー情報を削除 */
